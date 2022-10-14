fc1070c659c2242c731c32b37a2e10e6079ac74f
Awino2000 <awinoemanuel2000@gmail.com>
2022-10-14 18:48:31 +0300

    changed title, logo and background color

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Animated Robot")
+icon = pygame.image.load("robot.png")
+pygame.display.set_icon(icon)
+
 #Game loop
 running = True
 while running:
@@ -13,4 +18,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
+    #RGB
+    screen.fill((255, 255, 0))
+    pygame.display.update()
